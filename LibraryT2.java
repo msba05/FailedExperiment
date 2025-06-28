1f56b14f797d0cb70eb6bd6beee48d9df328ab9a
mariam :D <mariam.sonnah@gmail.com>
2025-06-27 19:49:00 -0500

    Update LibraryT2.java

--- LibraryT2.java
+++ LibraryT2.java
@@ -1,86 +1,88 @@
 
-import java.util.ArrayList;
+/**
+ * @author Trevor Sanderson, Mariam Barry, & Brayden Stewart
+ * date 6/27/2025
+ * ArrayLists Vs. Arrays -> In AI training projects, arrays and ArrayLists are tools used to store data,
+ * but they work best in different situations. Arrays are good when you know exactly how much data you have,
+ * like when dealing with images that are always the same size. They’re fast and don’t use much memory,
+ * but you can’t easily change their size. On the other hand, ArrayLists are better when the amount of data can change,
+ * like collecting sentences from websites or adding new training examples.
+ * They’re easier to work with because you can add or remove items without worrying about size,
+ * but they can be a bit slower. Basically, use arrays for fixed data and ArrayLists for flexible changing data.
+ **/
+
 import java.util.Scanner;
-public class LibraryT2 {
+public class LibraryT2
+{
 
-    String[] bookArray = new String[5];
-    int bookCount = 0;
-    
-    ArrayList<String> bookList = new ArrayList<>();
-    ArrayList<String> genreList = new ArrayList<>();
+    public static void main(String[] args)
+    {
+        DynamicBookStorage dynamicInventory = new DynamicBookStorage();
+        Scanner kbd = new Scanner(System.in);
+        boolean run = true;
+        int choice;
 
-public static void main(String[] args) {
-    Scanner kbd = new Scanner (System.in);
-    boolean run = true;
-    
-    while (run){
-    System.out.println("Welcome to the Book Invenrtory System Menu"
-            + ", Please choose the corresponding number for your choice below.");
-    System.out.println("----------------------------------------------------------------------");
-    System.out.println("1. Display by book titles.");
-    System.out.println("2. Add book titles.");
-    System.out.println("3. Remove book titles.");
-    System.out.println("4. Search by title");
-    System.out.println("5. Display by genre");
-    System.out.println("6. Search by genre");
-    System.out.println("0. Exit");
-    
-    kbd.nextLine();
-    run = menuSelection(kbd);
-    }
-    kbd.close();
-    
-    }
+        while (run)
+        {
 
-static boolean menuSelection(Scanner kbd){
-    int choice = kbd.nextInt();
-    
-    /*   
-    switch (choice){
-        case 1:
-            System.out.println("Add book title: ");
-        String title = kbd.nextLine();
-            System.out.println("Add genre for this book: ");
-        String genre = kbd.nextLine();
-            dynamicInventory.addTitleAndGenre(title, genre);
-            System.out.println("Successfully added!");
-        break;
-        case 2: 
-            System.out.println("Search book title: ");
-        String title = kbd.nextLine();
-            dynamicInventory.SearchTitle(title);
-            System.out.println("Successfully added!");
-            break;
-        case 3: 
-            System.out.println("Search book genre: ");
-        String genre = kbd.nextLine();
-            dynamicInventory.SearchByGenre(genre);
-            System.out.println("Successfully added!");
-            break;
-        case 4: 
-            System.out.println(Remove book title: );
-        String title = kbd.nextLine();
-            dynamicInventory.removeBook(title);
-            System.out.println("Successfully removed!");
-            break;
-        case 5: 
-            System.out.println(Display books by genre: );
-        String genre = kbd.nextLine();
-            dynamicInventory.displayGenre(genre);
-            break;
-        case 6: 
-            System.out.println(Display books);
-            dynamicInventory.displayBooks();
-            break;
-        case 0: 
-            System.out.println("Exiting System. Goodbye.");
-            return false;
-        default:
-            System.out.println("Invalid Option. Try Again.");
+            System.out.println("\nWelcome to the Book Inventory System Menu"
+                    + ", Please choose the corresponding number for your choice below.");
+            System.out.println("----------------------------------------------------------------------");
+            System.out.println("[Press Enter to continue]");
+            kbd.nextLine();
+            System.out.println("1. Add book titles.");
+            System.out.println("2. Search by title.");
+            System.out.println("3. Search by genre.");
+            System.out.println("4. Remove Book");
+            System.out.println("5. Display by genre");
+            System.out.println("6. Display Books");
+            System.out.println("0. Exit");
+            choice = kbd.nextInt();
+
+            switch (choice) {
+                case 1:
+                    kbd.nextLine();
+                    System.out.println("Add book title: ");
+                    String title = kbd.nextLine();
+                    System.out.println("Add genre for this book: ");
+                    String genre = kbd.nextLine();
+                    dynamicInventory.addTitleAndGenre(title, genre);
+                    System.out.println("Successfully added!");
+                    break;
+                case 2:
+                    kbd.nextLine();
+                    System.out.println("Search book by title: ");
+                    String searchTitle = kbd.nextLine();
+                    dynamicInventory.searchTitle(searchTitle);
+                    break;
+                case 3:
+                    kbd.nextLine();
+                    System.out.println("Search book genre: ");
+                    String genreSearch = kbd.nextLine();
+                    dynamicInventory.searchByGenre(genreSearch);
+                    break;
+                case 4:
+                    kbd.nextLine();
+                    System.out.println("Remove book title: ");
+                    String removeTitle = kbd.nextLine();
+                    dynamicInventory.removeBook(removeTitle);
+                    System.out.println("Successfully removed!");
+                    break;
+                case 5:
+                    kbd.nextLine();
+                    System.out.println("Enter book Genre");
+                    String displayGenre = kbd.nextLine();
+                    dynamicInventory.displayGenre(displayGenre);
+                    break;
+                case 6:
+                    dynamicInventory.displayBooks();
+                    break;
+                case 0:
+                    System.out.println("Exiting System. Goodbye.");
+
+                default:
+                    System.out.println("Invalid Option. Try Again.");
+            }
+        }
     }
-    */ return true;
-     }
-    
 }
-
-
